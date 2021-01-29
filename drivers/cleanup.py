1c6da4cee731436b43bc3aeae676f404f65cb132
Aleksander Wieliczko <aleksander.wieliczko@moosefs.pro>
2021-01-29 15:21:23 +0100

    Added SM Driver for MooseFS
    Co-authored-by: Piotr Robert Konopelko <piotr.konopelko@moosefs.pro>
    Signed-off-by: Aleksander Wieliczko <aleksander.wieliczko@moosefs.pro>
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.fr>

--- drivers/cleanup.py
+++ drivers/cleanup.py
@@ -3208,7 +3208,7 @@ def normalizeType(type):
         type = SR.TYPE_LVHD
     if type in [
         "ext", "nfs", "ocfsoiscsi", "ocfsohba", "smb", "cephfs", "glusterfs",
-        "xfs", "zfs", "ext4"
+        "moosefs", "xfs", "zfs", "ext4"
     ]:
         type = SR.TYPE_FILE
     if type in ["linstor"]:
