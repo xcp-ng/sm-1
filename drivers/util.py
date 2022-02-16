e4fa58e917a6d5e090778cfd654383e966abfe9e
Ronan Abhamon <ronan.abhamon@vates.fr>
2022-02-16 18:28:06 +0100

    timeout_call returns the result of user function now
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.fr>

--- drivers/util.py
+++ drivers/util.py
@@ -851,7 +851,7 @@ def timeout_call(timeoutseconds, function, *arguments):
     signal.signal(signal.SIGALRM, handler)
     signal.alarm(timeoutseconds)
     try:
-        function(*arguments)
+        return function(*arguments)
     finally:
         signal.alarm(0)
 
