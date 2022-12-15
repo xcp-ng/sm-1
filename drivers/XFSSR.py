44b1b40ebacfb873e3beb8eaee22c3c72dc553d2
Ronan Abhamon <ronan.abhamon@vates.fr>
2022-12-15 10:52:28 +0100

    Remove `_isvalidpathstring` calls
    Since CA-370696 upstream commit, `_isvalidpathstring` is no longer available.
    So the device path given to a SR at creation is checked using specific commands:
      - `vgcreate` for XFS SR.
      - Normally the same idea for EXT4 but the creation is deprecated, so this change has
        no impact.
    
    CA-370696 upstream commit: e48c01c215f550e9df032744b3a5c7db4780276d
    
    Signed-off-by: Ronan Abhamon <ronan.abhamon@vates.fr>

--- drivers/XFSSR.py
+++ drivers/XFSSR.py
@@ -71,10 +71,6 @@ class XFSSR(FileSR.FileSR):
             raise xs_errors.XenError('ConfigDeviceMissing')
 
         self.root = self.dconf['device']
-        for dev in self.root.split(','):
-            if not self._isvalidpathstring(dev):
-                raise xs_errors.XenError('ConfigDeviceInvalid', \
-                      opterr='path is %s' % dev)
         self.path = os.path.join(SR.MOUNT_BASE, sr_uuid)
         self.vgname = EXT_PREFIX + sr_uuid
         self.remotepath = os.path.join("/dev",self.vgname,sr_uuid)
