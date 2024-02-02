ae46b75bfb96da941a99ca91d14b94bb9e3d1cb0
Robin Newton <robin.newton@cloud.com>
2024-02-02 15:08:32 +0000

    CA-387770 Improve error message for readonly shares
    This deals with the situation where an NFSSR or SMBSR is being attached,
    but that share it uses cannot be written to.
    * Make an explicit check that the share can be written to after it is
      mounted, raising a suitable error message if it's not. The check comes
      before the hard-link check, as this also writes to the share, and so
      would fail in this situation.
    * If the attach fails (for this, or any other, reason) after the share
      has been mounted, ensure that it's unmounted. This is because if it is
      left mounted then a subsequent attach may succeed, by virtue of the
      checks being skipped.
    
    Signed-off-by: Robin Newton <robin.newton@cloud.com>

--- drivers/FileSR.py
+++ drivers/FileSR.py
@@ -1074,6 +1074,20 @@ class SharedFileSR(FileSR):
     """
     NO_HARDLINK_SUPPORT = "no_hardlinks"
 
+    def _check_writable(self):
+        """
+        Checks that the filesystem being used by the SR can be written to,
+        raising an exception if it can't.
+        """
+        test_name = os.path.join(self.path, str(uuid4()))
+        try:
+            open(test_name, 'ab').close()
+        except OSError as e:
+            util.SMlog("Cannot write to SR file system: %s" % e)
+            raise xs_errors.XenError('SharedFileSystemNoWrite')
+        finally:
+            util.force_unlink(test_name)
+
     def _raise_hardlink_error(self):
         raise OSError(524, "Unknown error 524")
 
